ab17a55758206133bdccf18a2e70c991dab6098e
ZenWeb3 <samueledohoeket257@gmail.com>
2024-11-04 14:34:32 +0100

    created section files and imported them

---
+++ src/Components/index.js
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar";
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from './CustomerReviews'
+import Subscribe from "./Subscribe";
+import Footer from './Footer'
+
+export {
+    Navbar,
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
